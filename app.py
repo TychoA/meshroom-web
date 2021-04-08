2fcfb1f6a9bee9134e9e5009b46927d6b31e6ccd
TychoA <tycho.atsma@gmail.com>
2021-04-08 15:51:13 +0200

    update docker file and flask structure

--- app.py
+++ app.py
@@ -1,20 +1,8 @@
-# Dependencies
-from flask import Flask, render_template, jsonify, request
+# Include our application
+from app import app
 
-# Define the application and port
-# Flask accepts a static folder parameter in which the static js and css is located
-# You can refer to it using the static_url_path parameter which is the href
-# used in templates, e.g. static_url_path = '/app' can be referred to as /app/main.js
-app = Flask(__name__, static_folder='app', static_url_path='/app')
+# Define the port the application is running on
 port = 8000
-
-# Define a route which will capture all routes and link them back to index
-@app.route('/', defaults = {'path': ''})
-@app.route('/<path:path>')
-def index(path):
-
-    # Return the index page. By default, Flask will look into a /templates folder
-    return render_template('index.html')
 
 # When this script is run directly, start the flask application
 if __name__ == "__main__":
---
+++ app/views.py
@@ -0,0 +1,11 @@
+# Include our application
+from app import app
+
+# Define a route which will capture all routes and link them back to index
+@app.route('/', defaults = {'path': ''})
+@app.route('/<path:path>')
+def index(path):
+
+    # Return the index page. By default, Flask will look into a /templates folder
+    return render_template('index.html')
+
